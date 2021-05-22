6771749342cce7e8a55d31f91d66d67a7bb8d00a
Aditya Bora <45680706+msaat366@users.noreply.github.com>
2021-05-22 16:43:45 +0530

    shit update

--- JS/test.js
+++ JS/test.js
@@ -28,7 +28,7 @@ const pageLength = 5;
 let offset = 0;
 let lon = 0;
 let lat = 0;
-let count = 0;
+
 
 function onShowPOI(data) {
   setTimeout(() => {
@@ -37,11 +37,21 @@ function onShowPOI(data) {
   
   const poiImg1 = document.getElementById('poi-img-1');
   const poiImg2 = document.getElementById('poi-img-2');
+  const poiText = document.getElementsByClassName('poi-text');
+
+  console.log(poiText);
+  /* poiText1.innerHTML */
   
   if (data.preview) {
+     document.getElementById('modal_dest').style.display = 'block';
     data_s.push(data);
     poiImg1.src = data_s[0].preview.source;
     poiImg2.src = data_s[1].preview.source;
+    poiText[0].innerHTML = "<h3>" + data_s[0].name + "</h3>"
+    poiText[0].innerHTML += '<p>' + data_s[0].wikipedia_extracts.text + '</p>';
+    poiText[1].innerHTML = "<h3>" + data_s[1].name + "</h3>"
+    poiText[1].innerHTML += '<p>' + data_s[1].wikipedia_extracts.text + '</p>';
+    
   } else {
     console.log("not found");
   }
@@ -53,6 +63,12 @@ function loadList() {
     'radius',
     `radius=30000&limit=${pageLength}&offset=${offset}&lon=${lon}&lat=${lat}&rate=2&format=json&kinds=museums,historic,natural,architecture`
   ).then(function (data) {
+    if (data.length == 0) {
+      alert("No places found")
+    }
+    else {
+      alert('loading Please Wait');
+    }
     data.forEach((item) => {
       apiGet('xid/' + item.xid).then((info) => {
         onShowPOI(info)
@@ -75,15 +91,14 @@ function loadList() {
 document
   .getElementById('search-d')
   .addEventListener('click', function (event) {
-    let name = document.getElementById('text-box').value;
-    console.log(name);
-    data_s = []
+    var name = document.getElementById('text-box').value;
+    document.getElementsByClassName('hl-name')[0].innerHTML = name;
+    data_s = [];
     apiGet('geoname', 'name=' + name).then(function (data) {
-      console.log(data);
+      console.log(data);  
       name.value = "";
-      //let message = 'Name not found';
       if (data.status == 'OK' && data.country == 'IN') {
-        document.getElementById('modal_dest').style.display = 'block';
+        
         message = data.name;
         lon = data.lon;   
         lat = data.lat;
@@ -91,14 +106,8 @@ document
       } else {
         alert("Sorry, but we can't seem to find this place");
       }
-      /* document.getElementById('info').innerHTML = `<p>${message}</p>`; */
     });
     event.preventDefault();
   });
 
-/* document
-  .getElementById('next_button')
-  .addEventListener('click', function () {
-    offset += pageLength;
-    loadList();
-  }); */
+
